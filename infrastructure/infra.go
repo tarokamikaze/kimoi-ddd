53f58177708dc28a8b72f2f0e2d6c55759172401
TaroKamikaze <ms06saz@gmail.com>
2019-04-25 00:50:44 +0900

    added comments

--- infrastructure/infra.go
+++ infrastructure/infra.go
@@ -18,6 +18,13 @@ type (
 func fromEntity(e domain.EntityUser) ActiveRecordUser {
 	rt := reflect.ValueOf(e)
 	return ActiveRecordUser{
+		// golangのメジャーORMライブラリは、public propertyしかマッピングしてくれない。
+		// 一方、データ整合性を担保するためにはDDDのEntityのproperty を一部 package private にする必要がある
+		// つまりActive Record(DTO?) 的な Struct (Class) はインフラ層で定義して、public propertyな状態にする必要がある
+
+		// 一方でDDD的には、ドメイン層からインフラ層への依存はご法度。
+		// つまり、インフラ層でEntityのprivate propertyから無理やり値を取得するしかない。
+		// つらい
 		ID:       rt.FieldByName("id").String(),
 		MailAddr: rt.FieldByName("mailAddr").String(),
 	}
